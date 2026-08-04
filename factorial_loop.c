9e8a4c7bc86d2bb72bc4127758d912d53853d52a
k0001-cmd <kushal1.manna@stu.adamasuniversity.ac.in>
2026-08-04 11:26:36 +0530

    Create factorial_loop.c

---
+++ factorial_loop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+ int factorial( int N) {
+    int fact = 1, i;
+
+    for (i = 1; i <= N; i++) {
+        fact *= i;
+    }
+
+    return fact;
+}
+
+int main()
+{
+    int N;
+    printf("Enter number");
+    scanf("%d",&N);
+    int fact = factorial(N);
+    printf("Factorial of %d is %d", N, fact);
+    return 0;
+}
