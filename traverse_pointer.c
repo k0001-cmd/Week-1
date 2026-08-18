c6e9f20ef8221075150a4b2221ad6ebe2313b18e
k0001-cmd <kushal1.manna@stu.adamasuniversity.ac.in>
2026-08-18 11:13:27 +0530

    Implement array traversal using pointers
    Added a program to traverse an array using pointers.

---
+++ traverse_pointer.c
@@ -0,0 +1,18 @@
+//WAP to traverse an array using pointer.
+#include <stdio.h>
+
+int main()
+{
+    int a[]={1,23,40,55,233,55,33,86,2};
+
+    int * p;
+
+    p=&a[0];
+    
+    for(int i=0;i<8;i++)
+    {
+        printf("%d\t",p[i]);
+    }
+
+    return 0;
+}
